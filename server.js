179d96d280d6bd4369660d24187896250a350a13
Alex Seeley <alexseeley134@gmail.com>
2024-10-10 14:26:03 -0600

    Complete challenge

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
+const fs = require("node:fs");
 const http = require("node:http");
 
-const server = http.createServer();
+const indexServer = http.createServer((req, res) => {
+  let path;
 
-server.on("request", (request, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World!</h1>");
+  switch (req.url) {
+    case "/":
+      path = "./index.html";
+      break;
+
+    case "/about":
+      path = "./about.html";
+      break;
+    case "/contact-me":
+      path = "./contact-me.html";
+      break;
+
+    default:
+      path = "./404.html";
+  }
+
+  fs.stat(path, (error, stats) => {
+    if (error) {
+      res.writeHead(404);
+      res.end("File not found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+        "Content-Length": stats.size,
+      });
+
+      const fileStream = fs.createReadStream(path);
+      fileStream.pipe(res);
+      fileStream.on("Error", (error) => {
+        console.error("File Stream error: " + error);
+        res.statusCode(500);
+        res.end("Internal server error");
+      });
+    }
+  });
 });
 
-server.listen(8080);
+indexServer.listen(8080);
