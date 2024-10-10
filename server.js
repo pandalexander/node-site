9487f83a6b1e87402ab846c339a4e066595dd95b
Alex Seeley <alexseeley134@gmail.com>
2024-10-10 15:28:34 -0600

    Install express and empty server.js for refactor

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const fs = require("node:fs");
-const http = require("node:http");
+// const fs = require("node:fs");
+// const http = require("node:http");
 
-const indexServer = http.createServer((req, res) => {
-  let path;
+// const indexServer = http.createServer((req, res) => {
+//   let path;
 
-  switch (req.url) {
-    case "/":
-      path = "./index.html";
-      break;
+//   switch (req.url) {
+//     case "/":
+//       path = "./index.html";
+//       break;
 
-    case "/about":
-      path = "./about.html";
-      break;
-    case "/contact-me":
-      path = "./contact-me.html";
-      break;
+//     case "/about":
+//       path = "./about.html";
+//       break;
+//     case "/contact-me":
+//       path = "./contact-me.html";
+//       break;
 
-    default:
-      path = "./404.html";
-  }
+//     default:
+//       path = "./404.html";
+//   }
 
-  fs.stat(path, (error, stats) => {
-    if (error) {
-      res.writeHead(404);
-      res.end("File not found");
-    } else {
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-        "Content-Length": stats.size,
-      });
+//   fs.stat(path, (error, stats) => {
+//     if (error) {
+//       res.writeHead(404);
+//       res.end("File not found");
+//     } else {
+//       res.writeHead(200, {
+//         "Content-Type": "text/html",
+//         "Content-Length": stats.size,
+//       });
 
-      const fileStream = fs.createReadStream(path);
-      fileStream.pipe(res);
-      fileStream.on("Error", (error) => {
-        console.error("File Stream error: " + error);
-        res.statusCode(500);
-        res.end("Internal server error");
-      });
-    }
-  });
-});
+//       const fileStream = fs.createReadStream(path);
+//       fileStream.pipe(res);
+//       fileStream.on("Error", (error) => {
+//         console.error("File Stream error: " + error);
+//         res.statusCode(500);
+//         res.end("Internal server error");
+//       });
+//     }
+//   });
+// });
 
-indexServer.listen(8080);
+// indexServer.listen(8080);
